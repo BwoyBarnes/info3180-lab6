0aaa87dd22b4fab07edd67815d933af4734a7c2e
BwoyBarnes <odanebarnes@outlook.com>
2017-03-14 13:30:53 -0500

    Step 3 - Removing Items

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@ var app = angular.module('myShoppingList', []);
 
 app.controller('myCtrl', function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
+    
     $scope.addItem = function () {
         $scope.products.push($scope.addMe);
+    }
+    
+    $scope.removeItem = function (x) {
+        $scope.products.splice(x, 1);
     } 
 });
